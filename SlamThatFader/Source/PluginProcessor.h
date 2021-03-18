4e38ddcfe33a48d15630ac93bfdaa333a4f2664d
Emma Azelborn <emmaazelborn@gmail.com>
2021-03-18 17:55:54 -0400

    delete unused old param tracking stuff

--- SlamThatFader/Source/PluginProcessor.h
+++ SlamThatFader/Source/PluginProcessor.h
@@ -60,31 +60,6 @@ private:
     
     juce::AudioProcessorValueTreeState m_parameters;
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
-    
-    // TODO: look into JUCE's valueChanged() callback
-    struct ParamState {
-        ParamState()
-        : m_paramSet(false)
-        , m_value(0.f)
-        {}
-        
-        // returns true if value changed
-        bool set(float newVal)
-        {
-            bool changed = !m_paramSet || m_value != newVal;
-            m_paramSet = true;
-            m_value = newVal;
-            return changed;
-        }
-        
-        bool m_paramSet;
-        float m_value;
-    };
-    std::array<ParamState, SlamThatFader::ParamIndex::NumParams> m_paramStates;
-    
-    // set these parameters and check if they changed
-    bool didChange(std::vector<int> paramIDs);
-    bool paramsAllSet();
 
     // amplitude
     float m_prevGain{1.f};
